abb24e1132ddf9c56d7b615c029b4f2f2a3061cb
hczjl <63627296+hczjl@users.noreply.github.com>
2023-05-11 09:12:14 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #此脚本为手动更新，请关闭程序后使用！
 cd uptime-kuma
 git fetch --all
-git checkout 1.20.2 --force
+git checkout 1.21.3 --force
 #在replit shell中输入 sh update.sh 回车 以更新
 npm ci --production
 npm run download-dist
